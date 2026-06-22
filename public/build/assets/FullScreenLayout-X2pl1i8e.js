1d3b47ea6fdc994c55c22bfc571142ae8f33a3c7
fahrulakbar16 <fahrulakbar16@gmail.com>
2026-06-23 07:41:02 +0800

    chore: update build assets manifest for application components

--- public/build/assets/FullScreenLayout-X2pl1i8e.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./FlashMessage-BsVljvC4.js";import{d as s,o,a as r,b as t,r as n}from"./app-B7-0i4GL.js";const c={class:"min-h-screen"},d={__name:"FullScreenLayout",setup(_){return(e,l)=>(o(),s("div",c,[r("main",null,[t(a),n(e.$slots,"default")])]))}};export{d as _};
