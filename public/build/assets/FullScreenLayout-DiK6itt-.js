aba91764ab986ab2111761d6272314f20ec09d51
fahrulakbar16 <fahrulakbar16@gmail.com>
2026-07-21 17:59:13 +0800

    build asset

---
+++ public/build/assets/FullScreenLayout-DiK6itt-.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./FlashMessage-D0_7dT7i.js";import{d as s,o,a as r,b as t,r as n}from"./app-C_doj05H.js";const c={class:"min-h-screen"},d={__name:"FullScreenLayout",setup(_){return(e,l)=>(o(),s("div",c,[r("main",null,[t(a),n(e.$slots,"default")])]))}};export{d as _};
