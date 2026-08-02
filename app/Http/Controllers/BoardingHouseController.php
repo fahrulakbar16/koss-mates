ed76b8884b5dfd4e1c806a3a49374837f556bcee
fahrulakbar16 <fahrulakbar16@gmail.com>
2026-08-03 06:39:14 +0800

    chore: remove debug logging from BoardingHouseController show method

--- app/Http/Controllers/BoardingHouseController.php
+++ app/Http/Controllers/BoardingHouseController.php
@@ -177,9 +177,6 @@ class BoardingHouseController extends Controller
             ->orderBy('name')
             ->get();
 
-        Log::info("Boarding House", [
-            "boardingHouse" => $boardingHouse,
-        ]);
         return Inertia::render('Admin/BoardingHouses/Show', [
             'boardingHouse' => $boardingHouse,
             'rooms' => $rooms,
