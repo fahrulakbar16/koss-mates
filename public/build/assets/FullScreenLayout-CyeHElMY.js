bd55142c649cebe040fdb7e23fc927123874e2fd
fahrulakbar16 <fahrulakbar16@gmail.com>
2026-08-25 16:28:18 +0800

    chore: rebuild application assets by updating project component build files

--- public/build/assets/FullScreenLayout-CyeHElMY.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./FlashMessage-DzjYmaCk.js";import{d as s,o,a as r,b as t,r as n}from"./app-DQ9U4CGX.js";const c={class:"min-h-screen"},d={__name:"FullScreenLayout",setup(_){return(e,l)=>(o(),s("div",c,[r("main",null,[t(a),n(e.$slots,"default")])]))}};export{d as _};
